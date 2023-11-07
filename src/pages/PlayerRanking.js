d3232e1dfcad3f4508827aa498692da623d93d69
wozniackia <wozniackia@gmail.com>
2023-11-07 02:31:53 +0100

    player ranking

--- src/pages/PlayerRanking.js
+++ src/pages/PlayerRanking.js
@@ -1,18 +1,25 @@
+import { useState, useEffect } from "react";
+import BackendUri from "../backend-helper";
 import Player from "../components/Player";
 import "../styles/Ranking.css";
 
 function Ranking() {
-  const ranking = [
-    {name: "Antoni Woźniacki",wins: "1"},
-    {name: "Kuba Buba",wins: "11"},
-    {name: "Dzonson",wins: "9000"},
-    {name: "Jakub Karpiński",wins: "10"}
-];
+
+  const [ranking, setRanking] = useState([]);
+
+    useEffect(() => {
+        fetch(`${BackendUri()}/api/v1/player/top`)
+          .then((response) => response.json())
+          .then((data) => {
+            setRanking(data)
+          })
+          .catch((error) => console.log(error));
+      }, []);
 
   return (
     <div className="ranking">
       <ul className="ranking-list">
-        {
+        { 
           ranking.sort((a, b) => {
             if (a.wins > b.wins) {
               return -1;
@@ -23,7 +30,7 @@ function Ranking() {
             }
           })
           .map((person, index) => (
-            <Player number={index+1} name={person.name} wins={person.wins}/>
+            <Player number={index+1} name={`${person.firstName} ${person.lastName}`} wins={person.wins}/>
           ))
         }
       </ul>
