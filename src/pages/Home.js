139b14bae0df60a88e0719ebc7dbf4232a252a36
wozniackia <wozniackia@gmail.com>
2023-11-11 07:02:36 +0100

    Dynamic background images

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,35 @@
 import '../styles/Home.css';
+import { useState } from 'react';
 import logo from '../lledh.png';
+import background from '../images/Island.jpg'
 
 function Home() {
+
+  const [backgroundImage, setBackgroundImage] = useState('');
+
+  function importAll(r) {
+    let images = {};
+    r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
+    return images;
+  }
+  
+  const images = importAll(require.context('../images', false, /\.(png|jpe?g|svg)$/));
+
+  setTimeout(() => {
+    setBackgroundImage(getNewBackgroundImage())
+  }, "10000");
+
+  function getNewBackgroundImage() {
+    let newImage = `url(${images[Object.keys(images)[ Object.keys(images).length * Math.random() << 0]]})`;
+    while (newImage == backgroundImage) {
+      newImage = `url(${images[Object.keys(images)[ Object.keys(images).length * Math.random() << 0]]})`;
+    }
+    return newImage;
+  }
+  
+
   return (
-    <div className='App'>
+    <div className='App' id='Home' style={{backgroundImage: backgroundImage}}>
       <img src={logo} className="hero-image" alt="logo" />
     </div>
   );
